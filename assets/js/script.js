fa69f1635f5ac84e087af53b1ad4bbf73c7bd888
Jacob Lysaught <jacob.lysaught@gmail.com>
2022-07-17 08:34:52 -0500

    polished css and manipulated moment.js to display correct dates on forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ let weather = {
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
         console.log(name,icon,description,temp,humidity,speed);
-        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".city").innerText = "Weather  in  " + name;
         document.querySelector(".weather-icon").src = "http://openweathermap.org/img/wn/" + icon +"@2x.png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + " °C";
@@ -25,23 +25,33 @@ let weather = {
     }
 };
 
+const searchbar = document.querySelector(".searchbar");
+
 document.querySelector(".searchBtn").addEventListener("click",  function () {
+    const searchText = searchbar.value;
+    console.log(searchText);
+    // localStorage.setItem(searchText, );
     weather.search();
 })
 
+// listens for enter key to be pressed while in the searchbar then calls search function which changes the city
 document.querySelector(".searchbar").addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
+        const searchText = searchbar.value;
+        console.log(searchText);
+        // localStorage.setItem(searchText, );
         weather.search();
     }
 })
 
-
 // sets the current date at the top of the page
-var now = moment();
-$("#currentDay").text(now.format("MMMM Do YYYY, h:mm:ss a"));
-// makes the time at the top of the page live 
-window.setInterval(function () {
-    $('#currentDay').html(moment().format("MMMM Do YYYY, h:mm:ss a"))
-}, 1000);
+let today = moment();
+$("#currentDay").text(today.format("MMMM Do YYYY"));
+$(".card-title").text(today.format("L"));
 
-var currentHour = moment().hour();
+// displays time added by one from one another to create the 5day forecast
+document.querySelector("#day-1").innerText = today.add(1, "days").format("L");
+document.querySelector("#day-2").innerText = today.add(1, "days").format("L");
+document.querySelector("#day-3").innerText = today.add(1, "days").format("L");
+document.querySelector("#day-4").innerText = today.add(1, "days").format("L");
+document.querySelector("#day-5").innerText = today.add(1, "days").format("L");
